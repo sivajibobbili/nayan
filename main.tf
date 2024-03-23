720024dcae34d8c955a21f1f8155af9c7a62ba23
sivajibobbili <162467313+sivajibobbili@users.noreply.github.com>
2024-03-23 13:19:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,8 +6,6 @@ resource "aws_instance" "ec2insta" {
     instance_type = "t2.micro"
     ami = "ami-0c101f26f147fa7fd"
     key_name =  "keypair89"
-    
-    subnet_id = aws_subnet.dev.id
     associate_public_ip_address = true
     tags = {
       Name="mysubnetpub"
